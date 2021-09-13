52865c381dec82768284d21eb790857d12f89313
Jonah Nisenson <jsnisenson@gmail.com>
2021-09-13 00:08:50 -0500

    update for subfolder

--- README.md
+++ README.md
@@ -1,33 +1,25 @@
-# Convert images on the fly to WebP from jpg, png, jpeg in an S3 bucket using an Amazon Lambda function
-Instead of creating WebP images from existing images in a batch operation, create them at runtime from requests. This has the advantage of only creating those WebP images if the original image is requested.
+# Convert images on the fly to WebP from jpg, png, jpeg using CloudFront Origin Response Lamdba Edge and S3 Bucket on AWS
+
+Instead of creating WebP images from existing images in a batch operation, create them at runtime from requests using an AWS CloudFront Origin Response lambda. This has the advantage of only creating those WebP images if the original image is requested.
 Any stale images that may be in the S3 bucket but not used on the website will therefore not have an associated WebP image created and therefore save additional costs for S3 bucket space.
 
-Blog Posts - This is a combination of several blog posts mashed together with some additiona modifications.
-1. [Resize Images on the Fly with Amazon S3, AWS Lambda, and Amazon API Gateway](https://aws.amazon.com/blogs/compute/resize-images-on-the-fly-with-amazon-s3-aws-lambda-and-amazon-api-gateway/) - this doesn't deal with converting to WebP but does have much of the logic for redirects and the Lambda function
-2. [Converting Images to WebP from CDN](https://blog.nona.digital/converting-images-to-webp-from-cdn/)  - this deals with converting to WebP from .jpg, .png, .jpeg but assumes you are using CloudFront
+## Additional steps for CloudFront Distribution:
 
-Additional steps for S3 bucket:
-1. Follow steps outlined in #1 above to create the S3 bucket and Enable Static Website Hosting (needed so we can use redirects on 404)
-2. The Redirect Rules should look like this which needs to be JSON (XML is no longer accepted). Replace __HOSTNAME_GATEWAY__API__ with the value you get as instructed in #1 of Blog Posts.
-Note that `ReplaceKeyPrefixWith` value will be the path after your hostname in the URL of the Gateway API. Note also that you need to add `?key=` suffix and the S3 Object key will automatically be added.
-```
-[
-    {
-        "Condition": {
-            "HttpErrorCodeReturnedEquals": "404"
-        },
-        "Redirect": {
-            "HostName": "__HOSTNAME_GATEWAY__API__",
-            "HttpRedirectCode": "307",
-            "Protocol": "https",
-            "ReplaceKeyPrefixWith": "default/generate-webp?key="
-        }
-    }
-]
-```
+1. This assumes you already have the S3 bucket created
+
+## Additional steps for CloudFront Distribution:
+
+1. 
 
 Additional tips for Lambda function:
+
 1. For Linux distro of the Lambda function, to zip the repo contents without adding the annoying subfolder with same name as the repo folder (which will cause the Lambda func to not work on upload), run the following from the root of the repo to have the zip file created at the same level as the repo (not as a child). Replace `webp-func` withn name of desired zip file.
+
 ```
 zip -r ../webp-func.zip .
 ```
+
+Blog Posts Inpiration - The basic idea was taken from the below but modified heavily.
+
+1. [Resize Images on the Fly with Amazon S3, AWS Lambda, and Amazon API Gateway](https://aws.amazon.com/blogs/compute/resize-images-on-the-fly-with-amazon-s3-aws-lambda-and-amazon-api-gateway/) - this doesn't deal with converting to WebP but does have much of the logic for redirects and the Lambda function
+2. [Converting Images to WebP from CDN](https://blog.nona.digital/converting-images-to-webp-from-cdn/) - this deals with converting to WebP from .jpg, .png, .jpeg but assumes you are using CloudFront
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
 
+// This is meant to be used for an Origin Response Lambda Edge function
 // based on this https://aws.amazon.com/blogs/compute/resize-images-on-the-fly-with-amazon-s3-aws-lambda-and-amazon-api-gateway/
 // and this https://blog.nona.digital/converting-images-to-webp-from-cdn/
 // This could be improved if we can pass the base image file type extension to lambda as a querystring parameter which we can do with CloudFront
-//   but not sure how to do with S3 Redirect rule on 404
 const path = require("path");
 const AWS = require("aws-sdk");
 
@@ -12,90 +12,117 @@ const S3 = new AWS.S3({
 });
 
 const Sharp = require("sharp");
-const BUCKET = process.env.BUCKET;
-const URL = process.env.URL;
 const QUALITY = 75;
+// 403 or 404 could be returned if a file is not found in the S3 bucket depending on how the bucket is set up
+var notFoundCodes = new Set([403, 404]);
 
 exports.handler = async (event, context, callback) => {
-  const key = event.queryStringParameters.key;
-  const match = key.match(/([^]+)(.webp$)/); // 1st capture group is everything before the extension
-  const objectNameSansExt = match && match[1];
-  let response = {};
+  const { request, response, config } = event.Records[0].cf;
+  const { uri, origin } = request;
+  const { distributionDomainName } = config;
+  const headers = response.headers;
+  const BUCKET = origin.s3.domainName.split(".s3.")[0]; // domainName is of the form <bucket-name>.s3.<location>
+  const URL = "https://" + distributionDomainName;
+  console.info("BUCKET: " + BUCKET);
+  console.info("URL: " + URL);
+  console.info("MM EVENT\n" + JSON.stringify(event, null, 2));
 
-  if (!objectNameSansExt) {
-    //Check if requested file is .webp
-    callback(null, {
-      statusCode: "404",
-      headers: {},
-      body: "",
-    });
+  console.info("response.status:" + response.status);
+
+  // early return to continue the request if not a 404 or 403 (S3 will return a 403 instead of 404 unless specific public settings are set)
+  if (!notFoundCodes.has(Number(response.status))) {
+    console.info("is 403/404 so early return");
+    callback(null, response);
     return;
   }
 
-  try {
-    const s3keyToCreate = objectNameSansExt + ".webp";
+  console.info("past not 403/404 early return");
 
-    var params = {
-      Bucket: BUCKET,
-      Prefix: objectNameSansExt,
-    };
+  // if request is for .webp, create the image if the jpg, jpeg, or png version exists
+  if (/([^]+)(.webp$)/.test(uri)) {
+    console.info("webp image requeted");
+    const match = uri.match(/([^]+)(.webp$)/); // 1st capture group is everything before the extension
+    let objectNameSansExt = match && match[1];
+    // Remove leading and trailing slash (if they exist) on key b/c we are pulling the key from the uri
+    objectNameSansExt = `${objectNameSansExt.replace(/\/$/g, "").replace(/^\/+/g, "")}`;
+    // ${URL.replace(/\/$/g, "")}/${uri.replace(/^\/+/g, "")}`
+    console.info("objectNameSansExt: " + objectNameSansExt);
 
-    var bucketFiltered = await S3.listObjectsV2(params).promise();
-    var contents = bucketFiltered.Contents;
+    try {
+      const s3keyToCreate = objectNameSansExt + ".webp";
 
-    if (contents && contents.length === 0) {
-      // There is no file with the key prefix
-      callback(null, {
-        statusCode: "404",
-        headers: {},
-        body: "",
-      });
-      return;
-    }
-
-    let foundOrigFile = false;
-
-    // iterate over possible files b/c we don't know if the jpg, png, or jpeg exists nor which one it could be
-    for (let i = 0; i < contents.length; i++) {
-      const objKey = contents[i].Key;
-      const ext = objKey.match(/(\.jpg|\.png|\.jpeg)$/g);
-
-      if (!ext) continue;
-
-      const s3keyLookup = objectNameSansExt + ext;
-      const bucketResource = await S3.getObject({ Bucket: BUCKET, Key: s3keyLookup }).promise();
-      const sharpImageBuffer = await Sharp(bucketResource.Body)
-        .webp({ quality: +QUALITY })
-        .toBuffer();
-
-      await S3.putObject({
-        Body: sharpImageBuffer,
+      var params = {
         Bucket: BUCKET,
-        ContentType: "image/webp",
-        CacheControl: "max-age=31536000",
-        Key: s3keyToCreate,
-        StorageClass: "STANDARD",
-      }).promise();
+        Prefix: objectNameSansExt,
+      };
 
-      response.statusCode = 301;
-      // header location for redirect. Remove trailing slash on URL and any leading / if exists on key
-      response.headers = { location: `${URL.replace(/\/$/g, "")}/${key.replace(/^\/+/g, "")}` };
-      response.body = "";
-      foundOrigFile = true;
-      break;
-    }
+      var bucketFiltered = await S3.listObjectsV2(params).promise();
+      var contents = bucketFiltered.Contents;
 
-    if (!foundOrigFile) {
-      callback(null, {
-        statusCode: "404",
-        headers: {},
-        body: "",
-      });
-      return;
+      if (!contents || (contents && contents.length === 0)) {
+        console.info("No contents early return");
+        // There is no file with the key prefix
+        callback(null, response);
+        return;
+      }
+
+      // iterate over possible files b/c we don't know if the jpg, png, or jpeg exists nor which one it could be
+      for (let i = 0; i < contents.length; i++) {
+        const objKey = contents[i].Key;
+        const ext = objKey.match(/(\.jpg|\.png|\.jpeg)$/g);
+
+        if (!ext) continue;
+
+        const s3keyLookup = objectNameSansExt + ext;
+        const bucketResource = await S3.getObject({
+          Bucket: BUCKET,
+          Key: s3keyLookup,
+        }).promise();
+        const sharpImageBuffer = await Sharp(bucketResource.Body)
+          .webp({ quality: +QUALITY })
+          .toBuffer();
+
+        // all webp images will be in a root folder matching the dir structure of rest of the images, only one level down
+        // E.g. a .jpg at /static/example.jpg will get an analog webp at /webp/static/example.jpg
+        const newWebpObj = "webp/" + s3keyToCreate;
+
+        await S3.putObject({
+          Body: sharpImageBuffer,
+          Bucket: BUCKET,
+          ContentType: "image/webp",
+          CacheControl: "max-age=31536000",
+          Key: newWebpObj,
+          StorageClass: "STANDARD",
+        }).promise();
+
+        response.status = 200;
+        response.body = sharpImageBuffer.toString("base64");
+        response.bodyEncoding = "base64";
+        response.headers["content-type"] = [{ key: "Content-Type", value: "image/webp" }];
+
+        //TODO: delete. this was taken from the webp conversion for S3 bucket when no CloudFront was in the pipeline
+        // response.status = 302;
+        // header location for redirect. Remove trailing slash on URL and any leading / if exists on key
+        // location: `${URL.replace(/\/$/g, "")}/${uri.replace(/^\/+/g, "")}`,
+        // response.headers = {
+        //   Location: URL + s3keyToCreate,
+        // };
+        // response.body = "";
+
+        // console.info("301 Url: " + URL + s3keyToCreate);
+        // callback(null, {
+        //   statusCode: 301,
+        //   headers: {
+        //     Location: URL + s3keyToCreate,
+        //   },
+        // });
+
+        break;
+      }
+    } catch (error) {
+      console.error(error);
+      callback(error);
     }
-  } catch (error) {
-    console.error(error);
-    callback(error);
   }
 
   callback(null, response);
